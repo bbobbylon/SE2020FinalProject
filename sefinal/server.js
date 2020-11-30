8243d473fdcc869550ad447868fe5099e03976fc
oliver <swoliverro@lewisu.edu>
2020-11-29 18:19:18 -0600

    tester

--- sefinal/server.js
+++ sefinal/server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users")
+const path = require("path"); // on top
+
 const app = express();
 // Bodyparser middleware
 app.use(
@@ -28,5 +30,14 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 //routes 
 app.use("/api/users" , users)
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+//const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+//app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build")); // change this if your dir structure is different
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+//app.set('port', process.env.PORT || 5000);
+//console.log("++++++++++++++++" + app.get('port'));
