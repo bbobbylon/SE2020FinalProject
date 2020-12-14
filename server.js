c6ee82d385eb8f105c0d57347c1681884085785d
bob <robertcoliver@lewisu.edu>
2020-12-14 09:24:15 -1000

    please work

--- server.js
+++ server.js
@@ -76,13 +76,14 @@ const port = process.env.PORT || 5000; // process.env.port is Heroku's port if y
 
 app.use(cors())
 app.use(express.json())
-
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build")); // change this if your dir structure is different
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
 
